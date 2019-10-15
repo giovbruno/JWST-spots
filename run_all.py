transmission spectrum calculation bug fixed

run_all.py
@@ -1,8 +1,7 @@
 import numpy as np
-import neptune_mdwarf
-import spotfree_IC
+import simulate_transit
+import transit_fit
 import spectra_fit
-import comparisons
 import os
 import pathlib
 from pdb import set_trace
@@ -16,21 +15,21 @@ def go(magstar, rstar, tstar, tumbra, tpenumbra, loggstar, rplanet, instr):
     pardict['instrument'] = str(instr) + '/'
     if not os.path.exists(pardict['project_folder'] + pardict['instrument']):
         os.mkdir(pardict['project_folder'] + pardict['instrument'])
-    pardict['attempt_data'] = 'p' + str(rplanet) + '_star' + str(rstar) + '_' \
-        + str(tstar) + '_' + str(loggstar) + '_spot' + str(tumbra) \
-        + '_' + str(tpenumbra) + '_mag' + str(magstar)
-    pardict['data_folder'] = pardict['project_folder'] + pardict['instrument'] \
-                            + 'simulated_data/' + pardict['attempt_data'] + '/'
+    pardict['case_folder'] = pardict['project_folder'] \
+                + pardict['instrument'] + 'p' + str(rplanet) + '_star' \
+                + str(rstar) + '_' + str(tstar) + '_' + str(loggstar) \
+                + '_spot' + str(tumbra) + '_' + str(tpenumbra) + '_mag' \
+                + str(magstar) + '/'
+    if not os.path.exists(pardict['case_folder']):
+        os.mkdir(pardict['case_folder'])
+    pardict['data_folder'] = pardict['case_folder'] + 'simulated_data/'
     if not os.path.exists(pardict['data_folder']):
         os.mkdir(pardict['data_folder'])
-    pardict['attempt_mcmc'] = pardict['attempt_data'] + '/'
-    #if not os.path.exists(pardict['attempt_mcmc']):
-    #    os.mkdir(pardict['attempt_mcmc'])
-    pardict['chains_folder']  = pardict['project_folder'] \
-            + pardict['instrument'] + 'MCMC/' + pardict['attempt_mcmc']
+    #pardict['attempt_mcmc'] = pardict['attempt_data'] + '/'
+    pardict['chains_folder']  = pardict['case_folder'] + 'MCMC/'
     if not os.path.exists(pardict['chains_folder']):
         os.mkdir(pardict['chains_folder'])
-    pardict['pandexo_out'] = pardict['data_folder'] + 'singlerun.pic'
+    pardict['pandexo_out'] = pardict['data_folder'] + 'singlerun.p'
     pardict['logfile_data'] = pardict['data_folder'] + 'logfile.log'
     pardict['logfile_chains'] = pardict['chains_folder'] + 'logfile.log'
     pardict['ldfile'] = pardict['project_folder'] \
@@ -46,30 +45,27 @@ def go(magstar, rstar, tstar, tumbra, tpenumbra, loggstar, rplanet, instr):
     pardict['rplanet'] = rplanet
 
     # Check
-    #if pathlib.Path(pardict['logfile_data']).exists() or \
-    #pathlib.Path(pardict['logfile_chains']).exists():
-    #    print('Log file already exists. Continue?')
-    #    pdb.set_trace()
-    #    os.system('rm ' + pardict['logfile_data'])
-    #    os.system('rm ' + pardict['logfile_chains'])
+    if pathlib.Path(pardict['logfile_data']).exists() or \
+    pathlib.Path(pardict['logfile_chains']).exists():
+        print('Log file already exists. Continue?')
+        set_trace()
+        os.system('rm ' + pardict['logfile_data'])
+        os.system('rm ' + pardict['logfile_chains'])
 
-    # These require python 2 (maybe)
-    #
-
-    res = 20
-    #neptune_mdwarf.generate_spectrum(pardict)
-    #lastchan = neptune_mdwarf.add_spots(pardict, res)
+    #simulate_transit.generate_spectrum(pardict)
+    #simulate_transit.add_spots(pardict, 20)
     #neptune_mdwarf.channelmerge(pardict, 3, 33)
 
-    # Create logfile for chains
-    #GP_white.GP_transit_spectro(pardict)
+    # Select channels and fit transit + spots
+    expchan = np.arange(40, 70, 10)
+    transit_fit.transit_spectro(pardict, expchan)
 
-    # Select channels
-    expchan = np.arange(22, 37, 3)
-    #expchan = np.arange(1, 78, 3)
-    #expchan = [1, 4, 8, 11, 15, 18, 22, 25, 28, 31, 34]
-    # First, spot-free fit
-    #spotfree_IC.transit_spectro(pardict, expchan)
+    # Fit derived transit depth rise with stellar models
+    spectra_fit.read_res(pardict, expchan, pardict['chains_folder'] \
+                       + 'contrast_plot_kurucz.pdf')
+
+    return
+
     # Then, spot models
         #spotmodels_IC.transit_spectro(pardict)
         #for i in np.arange(1, 74, 10):
@@ -90,10 +86,5 @@ def go(magstar, rstar, tstar, tumbra, tpenumbra, loggstar, rplanet, instr):
     #comparisons.comp_entropy([folder_root + 'case1_masked/', folder_root \
     #                   + 'case1_spots/'], ['masked', 'spots'])
     #comparisons.plot_contrasts(folder_root + 'case1_spots/')
-    #comparisons.plot_spectra([folder_root + 'case1_masked/', folder_root \\
+    #comparisons.plot_spectra([folder_root + 'case1_masked/', folder_root \
     #           + 'case1_spots/'], ['masked', 'spots'])
-
-    spectra_fit.read_res(pardict, expchan, pardict['chains_folder'] \
-                       + 'contrast_plot_kurucz.pdf')
-
-    return

simulate_transit.py
@@ -1,63 +1,66 @@
+# Generate transmission spectrum with PandExo and simulate transits with
+# spot occultations.
+
 import sys
 sys.path.append('/home/giovanni/archive/python/pandexo/')
 import warnings
 warnings.filterwarnings('ignore')
 import pandexo.engine.justdoit as jdi # THIS IS THE HOLY GRAIL OF PANDEXO
 import pandexo.engine.justplotit as jpi
-import numpy as np
-import os, sys, pickle
-from astropy.io import fits
 import matplotlib.pyplot as plt
-from pdb import set_trace
-from text_operations import printlog
-import blackbody as bb
 from scipy.interpolate import interp1d
-from astropy import units as u
-from astropy.analytic_functions import blackbody_lambda, blackbody_nu
-from astropy.utils.exceptions import AstropyDeprecationWarning
+import numpy as np
 import pysynphot
-#modfolder = '/home/giovanni/archive/Stellar_models/phxinten/HiRes/'
+import pickle, os
+from pdb import set_trace
 
 def generate_spectrum(pardict):
 
     print('TO ADD: 3D STELLAR MODELS FOR FGK STARS (NOT M)')
 
     exo_dict = jdi.load_exo_dict()
-    exo_dict['observation']['sat_level'] = 80  #saturation level in percent of full well
+    exo_dict['observation']['sat_level'] = 80  #saturation level in percent
+                                               #of full well
     exo_dict['observation']['sat_unit'] = '%'
     exo_dict['observation']['noccultations'] = 2   #number of transits
-    exo_dict['observation']['R'] = 20    #fixed binning. I usually suggest ZERO binning.. you can always bin lat34 without having to redo the calcualtion
-    exo_dict['observation']['baseline'] = 1.0    #fraction of time in transit versus out = in/out
+    exo_dict['observation']['R'] = None    #fixed binning. I usually suggest ZERO
+                                         #binning.. you can always bin later
+                                         #without having to redo the calcualtion
+    exo_dict['observation']['baseline'] = 1.0   #fraction of time in transit
+                                                #versus out = in/out
     exo_dict['observation']['baseline_unit'] = 'frac'
-    exo_dict['observation']['noise_floor'] = 20   #this can be a fixed level or it can be a filepath
+    exo_dict['observation']['noise_floor'] = 20   #this can be a fixed level
+                                                  #or it can be a filepath
     exo_dict['star']['type'] = 'phoenix'  #phoenix or user (if you have your own)
     exo_dict['star']['mag'] = pardict['magstar']     #magnitude of the system
-    exo_dict['star']['ref_wave'] = 1.25   #For J mag = 1.25, H = 1.6, K =2.22.. etc (all in micron)
+    exo_dict['star']['ref_wave'] = 1.25   #For J mag = 1.25, H = 1.6, K =2.22..
+                                          #etc (all in micron)
     exo_dict['star']['temp'] = pardict['tstar']     #in K
     exo_dict['star']['metal'] = 0.0        # as log Fe/H
     exo_dict['star']['logg'] = pardict['loggstar']
     exo_dict['star']['radius'] = pardict['rstar']
     exo_dict['star']['r_unit'] = 'R_sun'
 
-    #exo_dict['planet']['type'] = 'constant'
-    exo_dict['planet']['exopath'] = pardict['data_folder'] + 'exotransmit.dat'
+    exo_dict['planet']['type'] = 'constant'
+    #exo_dict['planet']['exopath'] = pardict['data_folder'] + 'exotransmit.dat'
     exo_dict['planet']['w_unit'] = 'um'
-    exo_dict['planet']['radius'] = pardict['rplanet']    #other options include "um","nm" ,"Angs", "secs" (for phase curves)
+    exo_dict['planet']['radius'] = pardict['rplanet'] #other options include
+                                                      #"um","nm" ,"Angs",
+                                                      #"secs" (for phase curves)
     exo_dict['planet']['r_unit'] = 'R_jup'
     exo_dict['planet']['transit_duration'] = 2.0
     exo_dict['planet']['td_unit'] = 'h'
     exo_dict['planet']['f_unit'] = 'rp^2/r*^2'
+
 
     #inst_dict = jdi.load_mode_dict('NIRSpec Prism')
     #inst_dict["configuration"]["detector"]["subarray"] = 'sub512'
     #inst_dict["configuration"]["detector"]["readmode"] = 'nrs'
 
     print('Starting TEST run')
-    #jdi.run_pandexo(exo_dict, ['NIRCam F444W'], save_file=True)
-    #jdi.run_pandexo(exo_dict, ['NIRSpec G140M'], save_file=True)
-    #jdi.run_pandexo(exo_dict, ['NIRSpec G235H'], save_file=True)
     jdi.run_pandexo(exo_dict, ['NIRSpec Prism'], save_file=True, \
-                    output_file=pardict['pandexo_out'])
+            output_file=pardict['pandexo_out'])
+    '''
     # Load in output from run
     out = pickle.load(open(pardict['pandexo_out'],'rb'))
 
@@ -81,8 +84,8 @@ def generate_spectrum(pardict):
     savefile = open(pardict['pandexo_out'], 'wb')
     pickle.dump(spec_obs, savefile)
     savefile.close()
-    set_trace()
-    return relsigma
+    '''
+    return #relsigma
 
 def add_spots(pardict, res):
 
@@ -94,19 +97,23 @@ def add_spots(pardict, res):
     print('TO ADD: GRANULATION') # To be considered as starspots, especially
                                  # for giant stars? Not this time
 
-    sys.path.append('KSint_wrapper/SRC/')
+    sys.path.append('../KSint_wrapper/SRC/')
     import ksint_wrapper_fitcontrast
 
     spec_obs = pickle.load(open(pardict['pandexo_out'], 'rb'))
-    #xobs, xobs_err, yobs, yobs_err = jpi.jwst_1d_spec(spec_obs, R=res, \
-    #            num_tran=10, model=False, plot=False)#, x_range=[.8,1.28])
-    xobs, yobs, xobs_err, yobs_err = spec_obs
-    xobs = np.array(xobs)#[0]
-    xobs_err = np.array(xobs_err)
-    yobs = np.array(yobs)#[0]
-    yobs_err = np.array(yobs_err)#[0]
+    xobs, yobs, yobs_err = jpi.jwst_1d_spec(spec_obs, R=res, \
+                num_tran=3, model=False, plot=False)
+                #, x_range=[.8,1.28])
+    #xobs, yobs, xobs_err, yobs_err = spec_obs
+    xobs = np.array(xobs)[0]
+    #xobs_err = np.array(xobs_err)
+    yobs = np.array(yobs)[0]
+    yobs_err = np.array(yobs_err)[0]
 
-    plt.errorbar(xobs, yobs, yerr=yobs_err, xerr=xobs_err, fmt = 'o')
+    plt.errorbar(xobs, yobs, yerr=yobs_err, fmt='o')
+    plt.xlabel('Wavelength [$\mu$m]', fontsize=16)
+    plt.ylabel('Transit depth', fontsize=16)
+    plt.title('Transmission spectrum output from PandExo', fontsize=16)
     plt.show()
     set_trace()
     # Add "white" light curve term
@@ -127,15 +134,16 @@ def add_spots(pardict, res):
 
     # Here, the last element of arange is the white light curve
     for i in np.arange(1, len(xobs) - 1):
-
         # Compute transit with two spots
         fix_dict = {}
+        '''
         if i < 10:   # Need to implement LD coefficients here
             fix_dict['c1'] = 0.25 #+ np.random.normal(loc = 0., scale = 0.01)#ua[i]#ub[i]
             fix_dict['c2'] = 0.2 #+ np.random.normal(loc = 0.0, scale = 0.01)#ua[i]#ub[i]
         else:
             fix_dict['c1'] = 0. #+ np.random.normal(loc = 0., scale = 0.01)#ua[i]#ub[i]
             fix_dict['c2'] = 0. #+ np.random.normal(loc = 0.0, scale = 0.01)#ua[i]#ub[i]
+        '''
         #print(fix_dict['c1'], fix_dict['c2'])
         fix_dict['prot'] = 11.8   # Hebrard+2012
         fix_dict['incl'] = 90.
@@ -143,8 +151,8 @@ def add_spots(pardict, res):
         fix_dict['lat'] = 12. # umbra
         fix_dict['latp'] = 12. # penumbra
         fix_dict['rho'] = 1.7*1.408
-        fix_dict['contrast1'] = 0.8
-        fix_dict['contrast2'] = 0.5
+        #fix_dict['contrast1'] = 0.8
+        #fix_dict['contrast2'] = 0.5
         fix_dict['P'] = 2.0
         fix_dict['i'] = 88.
         fix_dict['e'] = 0.
@@ -153,8 +161,8 @@ def add_spots(pardict, res):
         fix_dict['nspots'] = 1
         # Random distribution for spots parameters
         params = np.zeros(8 + 3) # second spot is penumbra
-        params[0] = yobs[i]**0.5
-        #print('Rp/R* =', params[0])
+        params[0] = yobs[i]**0.5 # kr
+        params[1] = 252.#270  # M
         # LD coeffs
         if xobs[i] < 3.:
             params[2], params[3] = 0.25 - 0.003*i, 0.2 - 0.003*i
@@ -162,35 +170,7 @@ def add_spots(pardict, res):
             params[2], params[3] = 0.01, 0.01
         # Long, size
         params[4], params[5] = 300., 5.0 #240., 5.0
-        params[8] = 12.#params[5]*2.4 # see reference in notes
-        params[7] = params[4] - 2
         # Contrast
-        #wlarr, contrast = bb.contrast_analytic(5000, 3800, 4000, 4800)
-        #set_trace()
-        #wlarr *= 1e6
-        '''
-        wl = np.arange(0.1, 10, 5e-3) * u.micrometer
-        tstar = 5000 * u.K
-        tspot = 4000 * u.K
-        with np.errstate(all='ignore'):
-            with warnings.catch_warnings():
-                warnings.simplefilter('ignore', AstropyDeprecationWarning)
-                flux_star = blackbody_lambda(wl, tstar)
-                flux_spot = blackbody_lambda(wl, tspot)
-                contrast = flux_spot/flux_star
-
-        wl = fits.open(modfolder \
-                    + 'WAVE_PHOENIX-ACES-AGSS-COND-2011.fits')[0].data   #AA
-        starm = fits.open(modfolder \
-          + 'lte' + str(pardict['tstar']) + '-' + "%.2f" %pardict['loggstar'] \
-          + '-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits')[0].data
-        spotm = fits.open(modfolder \
-           + 'lte' + str(pardict['tspot']) + '-' + "%.2f" %pardict['loggstar'] \
-           + '-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits')[0].data
-        penumbram = fits.open(modfolder \
-           + 'lte' + str(pardict['tpenumbra']) + '-' + "%.2f" %pardict['loggstar'] \
-           + '-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits')[0].data
-        '''
         wl = pysynphot.Icat('phoenix', pardict['tstar'], 0.0, \
                 pardict['loggstar']).wave
         starm = pysynphot.Icat('phoenix', pardict['tstar'], 0.0, \
@@ -205,24 +185,25 @@ def add_spots(pardict, res):
         chanright = (xobs[i] + 0.5*(xobs[i + 1] - xobs[i])) #* u.micrometer
         wlcenter = np.mean([chanleft, chanright]) #* u.micrometer
         wlbin = np.logical_and(wl*1e-4 >= chanleft, wl*1e-4 <= chanright)
-        params[6] = 1. - np.mean(contrast[wlbin])
+        params[6] = 1. - np.mean(contrast[wlbin]) # Contrast spot 1
+        # Spot 2
+        params[7] = params[4] - 2
+        params[8] = 12.#params[5]*2.4 # see reference in notes
         params[9] = 1. - np.mean(contrastp[wlbin])
-        print(params[6], params[9])
+        print('Channel', str(i), ', Kr =', np.round(params[0], 3), \
+                        'Contrast umbra =', np.round(params[6], 3))#, \
+                        #'Contrast penumbra =', np.round(params[9], 3))
         # Spot 2
         #params[5], params[6], params[7] = 0.1, 240., 4.0
-        # M
-        params[1] = 252.#270.
 
         tt = np.arange(0., 0.2, 60.*1./86400.)
-        #flag = np.logical_and(tt > 1.9, tt < 2.1)
-        transit = ksint_wrapper_fitcontrast.main(params, tt, fix_dict)#, fitcontrast = False)
+        transit = ksint_wrapper_fitcontrast.main(params, tt, fix_dict)
         # White noise
         transit *= np.random.normal(loc=1., scale=relsigma[i], size=len(tt))
         yerr = np.zeros(len(transit)) + np.mean(relsigma[i])
         #tt, transit, yerr = tt[flag], transit[flag], yerr[flag]
         # Delta x^2 = 2 x Delta x
         relsigma_i = np.zeros(len(tt)) + relsigma[i]/(2.*(yobs[i]**0.5))
-        print(i, chanleft, chanright, 1 - params[6], params[0])
         plt.close('all')
         plt.errorbar(tt, transit, yerr=relsigma[i], fmt='k.')#, capsize=2)
         '''
@@ -236,17 +217,17 @@ def add_spots(pardict, res):
         plt.xlabel('Time [d]', fontsize=16)
         plt.ylabel('Relative flux', fontsize=16)
         plt.title('Channel: ' + str(round(xobs[i], 3)) + '$\mu$m', fontsize=16)
-        plt.legend(loc='best', fontsize=16, frameon=False)
+        #plt.legend(loc='best', fontsize=16, frameon=False)
         plt.savefig(pardict['data_folder'] + 'transit_spots' + str(i) + '.pdf')
         savefile = open(pardict['data_folder'] + 'transit_spots' \
                         + str(i) + '.pic', 'wb')
-        #plt.show()
-        #set_trace()
-        #pickle.dump([tt, transit, relsigma_i], savefile)
         pickle.dump([tt, transit, yerr, xobs[i]], savefile)
         savefile.close()
 
-    return i#tt, transit
+    # Remove pandexo plot files
+    os.system('rm *html')
+
+    return #i#tt, transit
 
 def channelmerge(pardict, ch1, ch2):
     '''

spectra_fit.py
@@ -1,13 +1,14 @@
-# Compare spot fits with stellar stellar models.
+# Compare spot fits with stellar models.
+
 import numpy as np
-import glob
-import pickle
-from pdb import set_trace
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy.optimize import least_squares
+import rebin
 import pysynphot
-import plot_spectra
+import glob
+import pickle
+from pdb import set_trace
 
 def read_res(pardict, ind, plotname):
     '''
@@ -50,15 +51,14 @@ def read_res(pardict, ind, plotname):
     '''
 
     # Get spectra with starspot contamination and perform LM fit
-    for temp in np.arange(3000, 6000, 250):
-        ww, spec = plot_spectra.combine_spectra(pardict, [temp], 0.05)
+    for temp in np.arange(4000, 6000, 250):
+        ww, spec = combine_spectra(pardict, [temp], 0.05)
         ww/= 1e4
         spec*= A[:, 2].max()
         specint = interp1d(ww, spec)
         specA = specint(wl)
-        soln = least_squares(scalespec, 1., args=(specA, \
-                            A[:, 2], yerr))
-        print('Spectrum scaling factor:', soln.x)
+        soln = least_squares(scalespec, 1., args=(specA, A[:, 2], yerr))
+        print('Spectrum scaling factor:', soln.x[0])
         plt.plot(ww, soln.x*spec, label=str(temp))
     plt.legend(frameon=False, loc='best', fontsize=16)
     plt.xlabel('Wavelength [$\mu m$]', fontsize=16)
@@ -74,3 +74,55 @@ def scalespec(x, spec, y, yerr):
     Distance between model spectrum and data.
     '''
     return (x*spec - y)**2/yerr**2
+
+def combine_spectra(pardict, tspot, ffact, res=100.):
+    '''
+    Combines starspot and stellar spectrum.
+    Fit with Kurucz instead of Phoenix models 2/10/19
+
+    Input
+    -----
+    tspot: list (of effective temperatures)
+    '''
+
+    # This is in Angstroms
+    wave = pysynphot.Icat('k93models', pardict['tstar'], 0.0, \
+                pardict['loggstar']).wave
+    # Reduce resolution
+    wnew = rebin.rebin_wave(wave, res)
+    star = pysynphot.Icat('k93models', pardict['tstar'], 0.0, \
+                pardict['loggstar']).flux
+    rebstar, errstar = rebin.rebin_spectrum(star, wave, wnew)
+    # Increase errors (based on ETC?)
+    errstar += rebstar*abs(np.random.normal(loc=0., scale=5.5e-3, \
+                            size=len(errstar)))
+    #plt.errorbar(wnew/10., rebstar, yerr=errstar, label=str(tstar))
+
+    for i in tspot:
+        spot = pysynphot.Icat('k93models', i, 0.0, pardict['loggstar']).flux
+        specnew = spot#(1 - ffact)*star + ffact*spot
+        rebnew, errnew = rebin.rebin_spectrum(specnew, wave, wnew)
+        errnew += rebnew*abs(np.random.normal(loc=0., scale=5.5e-3,\
+                        size=len(errnew)))
+        # Compare plots
+        #plt.errorbar(wnew/10., rebnew, yerr=errnew, label=str(i))
+        # As Sing+2011
+        wref = np.logical_and(39000 < wnew, wnew < 41000)
+        spotref = rebnew[wref]
+        starref = rebstar[wref]
+        fref = 1. - np.mean(spotref/starref)
+        rise = (1. - rebnew/rebstar)/fref
+        '''
+        plt.errorbar(wnew/1e4, rise, label=r'$T_{\mathrm{eff}, \bullet}$ = '\
+                        + str(i) + 'K')
+
+    plt.legend(frameon=False)
+    plt.title('Stellar spectrum: ' + str(tstar) + ' K', fontsize=16)
+    #plt.plot([0.719, 0.719], [0.5, 9.5], 'k--')
+    plt.xlabel('Wavelength [$\mu$m]', fontsize=16)
+    plt.ylabel('Dimming factor', fontsize=16)
+    plt.show()
+    '''
+    flag = np.logical_or(np.isnan(wnew), np.isnan(rise))
+
+    return wnew[~flag], rise[~flag]

transit_fit.py
@@ -40,8 +40,7 @@ def transit_spectro(pardict, expchan):
     wm, ua, eua, ub, eub = np.loadtxt(pardict['ldfile'], unpack=True, \
                         skiprows=3)
 
-    # Band-integrated transit
-    # files in the data folder
+    # Band-integrated transit files in the data folder
     ll = len(glob.glob(pardict['data_folder'] + 'transits_spots*.pic'))
     #ll = len(wm) - 1
     whiteleft = wm.min() - np.diff(wm)[0]/2.
@@ -50,15 +49,12 @@ def transit_spectro(pardict, expchan):
                     + str(ua[-1]) + '-' + str(ub[-1]), logger)
     global ldw
     ldw = [ua[-1], ub[-1]]
-    # To save results
+    # Save results
     diz_res = {}
     for i in expchan:
         samples_blue, spot1 = transit_emcee(pardict, logger, int(i))
         diz_res[i] = spot1
     printlog('White light curve fit - OK', logger)
-
-    #correct_spectro(pardict)
-    #mcmc_spectro(pardict, logger)
 
     filespot = open(pardict['chains_folder'] + '/spots_signal.p', 'wb')
     pickle.dump(diz_res, filespot)
@@ -94,10 +90,6 @@ def transit_emcee(diz, logger, ind):
 
     flag = np.logical_and(t > flagext[0], t < flagext[1])
     datasav = [t.copy(), y.copy(), yerr.copy()]
-    #t, y, yerr = t[~flag], y[~flag], yerr[~flag]
-    #plt.errorbar(t, y, yerr = yerr, fmt = 'k.')
-    #plt.show()
-    #set_trace()
 
     bounds_model = []
     bounds_model.append((0.01, 0.2))
@@ -109,15 +101,10 @@ def transit_emcee(diz, logger, ind):
     bounds_model.append((-1., 1.))
     bounds_model.append((-1., 1.))
     bounds_model.append((-1,  1.))
-    #bounds_model.append((-1., 1.))
-    #bounds_model.append((-1., 1.))
     bounds_model.append((0.95, 1.05))
     bounds_model.append((1e-6, 1e-2)) # A
     bounds_model.append((0.07, 0.09)) # x0
     bounds_model.append((1e-6, 1e-2)) # sigma
-    #liminf = [0.01, 0.08, 6., 85., 0.01, 0.01, -1., -1., -1., -1., -1., 0.5]
-    #limsup = [0.2,  0.12, 9., 90., 0.99, 0.99,  1.,  1.,  1.,  1.,  1., 1.5]
-    #bounds = (liminf, limsup)
 
     kr, t0, aR, i, ua, ub, r0, r1, r2, C = 0.10, 0.08, 8., \
             88., ldw[0], ldw[1], -1e-3, 1e-4, -1e-4, 1.
@@ -129,11 +116,9 @@ def transit_emcee(diz, logger, ind):
     nll = lambda *args: -lnlike_white(*args)
     soln = minimize(nll, initial_params, jac=False, method='L-BFGS-B', \
                     args=(t, y, yerr), bounds=bounds_model, options=options)
-    #               method = 'Nelder-Mead', bounds=bounds_model, \
-    #                args=(t, y, yerr), options=options)
 
     print('Likelihood maximimazion results:')
-    print(soln)
+    #print(soln)
 
     # Initial error on parameters (prior)
     initial_err = np.copy(ierrw)